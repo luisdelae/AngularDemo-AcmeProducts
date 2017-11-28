24128405e531af0ca47252d75e53a98f1a930d8e
Luis <luisdelae@gmail.com>
2017-11-28 15:22:24 -0600

    Getting products by Id

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { ProductService } from './product.service'
 
 import { IProduct } from './product';
 
@@ -14,26 +15,18 @@ export class ProductDetailComponent implements OnInit {
     product: IProduct;
 
     constructor(private _route: ActivatedRoute,
-                private _router: Router) { }
+        private _router: Router,
+        private _producList: ProductService) { }
 
     ngOnInit() {
         let id = +this._route.snapshot.paramMap.get('id');
 
         this.pageTitle += `: ${id}`;
 
-        this.product = {
-            "productId": 1,
-            "productName": "Leaf Rake",
-            "productCode": "GDN-0011",
-            "releaseDate": "March 19, 2016",
-            "description": "Leaf rake with 48-inch wooden handle.",
-            "price": 19.95,
-            "starRating": 3.2,
-            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
-        }
+        let product = this._producList.getProduct(id);
 
-        //Add http logic for getting the product with matching id.
-
+        product.subscribe(data => this.product = data);
+        
     }
 
     onBack(): void {
--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/throw';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/map';
 
 import { IProduct } from './product';
 
@@ -11,20 +12,25 @@ import { IProduct } from './product';
 
 export class ProductService {
 
-  private _productUrl = './api/products/products.json';
+    private _productUrl = './api/products/products.json';
 
-  constructor(private _http: HttpClient) {}
+    constructor(private _http: HttpClient) {}
 
-  getProducts(): Observable<IProduct[]> {
+    getProducts(): Observable<IProduct[]> {
 
-    return this._http.get<IProduct[]>(this._productUrl)
-      .do(data => console.log('All: ' + JSON.stringify(data)))
-      .catch(this.handleError);
+        return this._http.get<IProduct[]>(this._productUrl)
+          .do(data => console.log('All: ' + JSON.stringify(data)))
+          .catch(this.handleError);
 
-  }
+    }
 
-  private handleError(err: HttpErrorResponse) {
-    console.log(err.message);
-    return Observable.throw(err.message);
-  }
+    getProduct(id: number): Observable<IProduct> {
+        return this.getProducts()
+            .map((products: IProduct[]) => products.find(p => p.productId === id));
+    }
+
+    private handleError(err: HttpErrorResponse) {
+        console.log(err.message);
+        return Observable.throw(err.message);
+    }
 }
